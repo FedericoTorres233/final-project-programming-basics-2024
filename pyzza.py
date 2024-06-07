db8edb71dd470482d618a07948232287fbeb3002
Federico Torres <federicoivantorres1@gmail.com>
2024-06-07 12:59:38 -0300

    docs: agregar comentarios

--- pyzza.py
+++ pyzza.py
@@ -1,9 +1,10 @@
 import sqlite3
 
-# Conectar a la base de datos SQLite (se creará si no existe)
+# conectarse a la base de datos SQLite
 conn = sqlite3.connect('pyzza.db')
 db = conn.cursor()
 
+# logo
 logo = """
   _____
  |  __ \\                   
@@ -15,7 +16,7 @@ logo = """
         |___/         
 """
 
-# Crear la tabla de productos si no existe
+# crear la tabla de productos si no existe
 db.execute('''
 CREATE TABLE IF NOT EXISTS productos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,6 +29,7 @@ CREATE TABLE IF NOT EXISTS productos (
 conn.commit()
 
 
+# promedio hace el toma una lista de productos y retorna el promedio (prod[1] es el precio)
 def promedio(lista):
     suma = 0
     for prod in lista:
@@ -35,12 +37,13 @@ def promedio(lista):
     return suma / len(lista)
 
 
+# maximo calcula el producto de maximo precio de una lista de productos
 def maximo(productos):
-    # Inicializar el mayor como el menor valor posible
+    # inicializar el mayor como el menor valor posible
     precio_mayor = 0
     mayor = ["", 0]
 
-    # Recorrer la lista y actualizar el valor del mayor si encontramos un valor mayor
+    # recorrer la lista y actualizar el valor del mayor si encontramos un valor mayor (elemento[1] es el precio)
     for elemento in productos:
         if elemento[1] > precio_mayor:
             mayor = elemento
@@ -49,12 +52,13 @@ def maximo(productos):
     return mayor
 
 
+# minimo calcula el producto de maximo precio de una lista de productos
 def minimo(productos):
-    # Inicializar el mayor como el menor valor posible
+    # inicializar el menor como el mayor valor posible
     precio_menor = 9999999999999999999
     menor = ["", 0]
 
-    # Recorrer la lista y actualizar el valor del mayor si encontramos un valor mayor
+    # recorrer la lista y actualizar el valor del menor si encontramos un valor menor (elemento[1] es el precio)
     for elemento in productos:
         if elemento[1] < precio_menor:
             menor = elemento
@@ -63,6 +67,7 @@ def minimo(productos):
     return menor
 
 
+# cargar_producto pide el ingreso de productos y sus características hasta que nombre sea vacío
 def cargar_producto():
     nombre = input("Ingrese el nombre del producto: ")
     while nombre != "":
@@ -70,10 +75,14 @@ def cargar_producto():
         cantidad = int(input("Ingrese la cantidad de " + nombre + ": "))
         moneda = input("Ingrese la criptomoneda con la que pagar " + nombre +
                        ": ")
+    
+        # insertar datos en la db
         db.execute(
             'INSERT INTO productos (nombre, precio, cantidad, moneda) VALUES (?, ?, ?, ?)',
             (nombre.lower(), precio, cantidad, moneda))
         conn.commit()
+
+        # mostrar datos cargados
         print("Producto " + nombre + " cuyo precio es $" + str(precio) + " x" +
               str(cantidad) + ", y que se compra con " + str(moneda) +
               ", se ha cargado exitosamente")
@@ -82,9 +91,13 @@ def cargar_producto():
         )
 
 
+# listar_productos genera una lista formateada de un producto y sus características
 def listar_productos():
+    # tomar datos en la db
     db.execute('SELECT nombre, precio, cantidad, moneda FROM productos')
     productos = db.fetchall()
+
+    # verifico si hay productos 
     if productos:
         print("-=============================-")
         print("|Listado de productos")
@@ -99,10 +112,13 @@ def listar_productos():
         print("No hay productos")
 
 
+# promedio_precios devuelve el promedio de los precios de todos los productos
 def promedio_precios():
+    # tomar datos en la db
     db.execute('SELECT nombre, precio FROM productos')
     lista = db.fetchall()
 
+    # checkeo si la lista está vacía
     if lista == ():
         print("No hay productos para calcular el promedio")
     else:
@@ -111,28 +127,43 @@ def promedio_precios():
               str(round(prom, 2)))
 
 
+# producto_mas_caro devuelve el mas caro de todos los productos
 def producto_mas_caro():
+    # tomar datos en la db
     db.execute('SELECT nombre, precio, cantidad, moneda FROM productos')
     productos = db.fetchall()
+
+    # calculo el máximo de la lista
     mas_caro = maximo(productos)
     print("El producto más caro es " + str(mas_caro[0]) +
           " con un precio de $" + str(mas_caro[1]) + " en " + mas_caro[3])
 
-
+# producto_mas_caro devuelve el mas barato de todos los productos
 def producto_mas_barato():
+    # tomar datos en la db
     db.execute('SELECT nombre, precio, cantidad, moneda FROM productos')
     productos = db.fetchall()
+
+    # calculo el minimo de la lista
     mas_barato = minimo(productos)
     print("El producto más barato es " + str(mas_barato[0]) +
           " con un precio de $" + str(mas_barato[1]) + " en " + mas_barato[3])
 
 
+# info_sobre_producto devuelve la información sobre un producto de manera formateada
 def info_sobre_producto(info):
+    # tomar datos en la db
     db.execute('SELECT nombre, precio, cantidad, moneda FROM productos')
     productos = db.fetchall()
+
+    # el contador se utiliza para comprobar si se encontró un producto
     contador = 0
+
+    # itero productos
     for prod in productos:
+        # itero cada característica
         for i in range(len(prod)):
+            # si la característica es igual a lo ingresado, se devuelve la información sobre ese producto
             if prod[i] == info.lower():
                 contador += 1
                 print("|------------------------------")
@@ -146,11 +177,12 @@ def info_sobre_producto(info):
         print("No se encontró el producto :/")
 
 
+# genera el prompt para volver a mostrar el menú
 def esperar_input():
     input("\nPresione Enter para continuar...")
     print("\n")
 
-
+# inicializo el menú
 def menu():
     opcion = ""
     while opcion != "g":
@@ -164,8 +196,8 @@ def menu():
         print("\tf) Producto más barato")
         print("\tg) Salir")
 
+        # analisis de opciones ingresadas
         opcion = input("Seleccione una opción: ")
-
         if opcion == 'a':
             cargar_producto()
         elif opcion == 'b':
@@ -192,8 +224,8 @@ def menu():
             esperar_input()
 
 
-# Ejecutar el menú
+# ejecutar el menú
 menu()
 
-# Cerrar la conexión a la base de datos al finalizar el programa
+# cerrar la conexión a la db por seguridad
 conn.close()
